ff6de46b50b0208bdec5ef3b03ce36aecda9681e
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-21 10:54:10 +0000

    Mon, Mar 21, 2022, 10:54 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,39 @@
 # TODO
+def main():
+    text = input("Text: ")
+
+    letters = count_letters(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+
+    L = letters * 100 / words
+    S = sentences * 100 / words
+
+    index = 0.0588 * L - 0.296 * S - 15.8
+    print(f"Grade: {round(index)}")
+
+def count_letters(text):
+    count = 0
+    for letter in text:
+        if letter.isalpha():
+            count += 1
+    return count
+
+def count_words(text):
+    count = 0
+    for letter in text:
+        if letter == " ":
+            count += 1
+    count += 1
+    return count
+
+def count_sentences(text):
+    count = 0
+    for letter in text:
+        if letter in [".", "!", "?"]:
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    main()
