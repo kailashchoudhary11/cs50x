2415dd2959abf1ba6b16a91d8dd01511cc103267
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-21 09:43:49 +0000

    Mon, Mar 21, 2022, 9:43 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,26 @@
 # TODO
-from cs50 import get_int
 
+from cs50 import get_string
 number = get_string("Number: ")
-print("VISA")
-print("AMEX")
-print("MASTERCARD")
-print("INVALID")
+lst = list()
+for i in range(len(number) - 2, -1, -2):
+    lst.append(str(int(number[i]) * 2))
 
+sum = 0
+
+for item in lst:
+    for c in item:
+        sum += int(c)
+
+for i in range(len(number) - 1, 0, -2):
+    sum += int(number[i])
+
+if sum % 10 == 0:
+    if number.startswith("4"):
+        print("VISA")
+    if number.startswith(("34", "37")):
+        print("AMEX")
+    if number.startswith(tuple(range(51, 56))):
+        print("MASTERCARD")
+else:
+    print("INVALID")
