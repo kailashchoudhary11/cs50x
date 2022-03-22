be8aced460105f7fce0c70586b14bf99f5a707ba
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-22 00:41:30 +0000

    Tue, Mar 22, 2022, 12:41 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -10,15 +10,19 @@ while True:
 count = 0
 while cents > 0:
     if cents >= 0.25:
+        print(cents)
         cents -= 0.25
         count += 1
     elif cents >= 0.10:
-        cents -= 0.10
+        print(cents)
+        cents -= 0.1
         count += 1
     elif cents >= 0.05:
+        print(cents)
         cents -= 0.05
         count += 1
     else:
+        print(cents)
         cents -= 0.01
         count += 1
 
