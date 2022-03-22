b83b613ff5f147d66646c9bc7d97a06158661eb3
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-22 00:34:18 +0000

    Tue, Mar 22, 2022, 12:34 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,4 +3,23 @@
 from cs50 import get_float
 
 while True:
-    
+    cents = get_float("Value: ")
+    if cents > 0:
+        break
+
+count = 0
+while cents > 0:
+    if cents >= 0.50:
+        cents -= 0.50
+        count += 1
+    elif cents >= 0.25:
+        cents -= 0.25
+        count += 1
+    elif cents >= 0.10:
+        cents -= 0.10
+        count += 1
+    else:
+        cents -= 0.01
+        count += 1
+
+print()
