9d3f6fdfdb04f78ce93996e9e9629bac82d7c996
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-30 13:58:32 +0000

    Wed, Mar 30, 2022, 1:58 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- checking the crimes that took place on 28th july 2021
+sqlite> SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
