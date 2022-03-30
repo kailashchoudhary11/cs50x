e38e2006cf996e9fc0647ca900704a4b723bb1dc
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-30 13:18:22 +0000

    Wed, Mar 30, 2022, 1:18 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter") INTERSECT SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp"));
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter") INTERSECT SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp"));
