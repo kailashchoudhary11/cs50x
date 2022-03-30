70c818373cfb967fdf682494e88829eb912b6db3
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-30 13:23:34 +0000

    Wed, Mar 30, 2022, 1:23 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958;
+SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958);
