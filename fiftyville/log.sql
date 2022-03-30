c408b3ac31ed1b61763d6e19bb086cf013d0168b
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-30 14:18:05 +0000

    Wed, Mar 30, 2022, 2:18 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,4 +7,4 @@ SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transc
 
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15;
 
-s WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
+SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
