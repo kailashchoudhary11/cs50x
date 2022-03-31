2faf13249fe0e1b5a1c76bd285aba269a022e5e2
Kailash Choudhary <kailashjat604@gmail.com>
2022-03-31 01:41:27 +0000

    Thu, Mar 31, 2022, 1:41 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,4 +19,4 @@ SELECT * FROM airports WHERE id = (SELECT destination_airport_id FROM flights WH
 
 SELECT * FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") ORDER BY hour LIMIT 1);
 
-T passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") ORDER BY hour LIMIT 1)) AND id IN ();
+SELECT * FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute <= 25 AND minute >= 15) AND phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id = (SELECT id FROM airports WHERE city = "Fiftyville") ORDER BY hour LIMIT 1)) AND id IN ();
