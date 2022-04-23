5f44df8b8471cbab060cdcbfe3105d8f96801eec
Kailash Choudhary <kailashjat604@gmail.com>
2022-04-23 12:16:59 +0000

    Sat, Apr 23, 2022, 12:16 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,13 +27,38 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        print(name, month, day)
+        db.execute(
+            "INSERT INTO birthdays(name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
 
-        return render_template("index.html")
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
 
 
+@app.route("/delete", methods=["POST"])
+def delete():
+    id = request.form.get("id")
+    db.execute("DELETE FROM birthdays WHERE id = ?", id)
+    return redirect("/")
+
+@app.route("/update", methods=["GET", "POST"])
+def update():
+    if request.method == "POST":
+        id1 = request.form.get("id")
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        print(id1, name, month, day)
+        db.execute("UPDATE birthdays SET name=?, month=?, day=? WHERE id = ?", name, month, day, id1)
+        return redirect("/")
+    id = request.args.get("id")
+    birthday = db.execute("SELECT * FROM birthdays WHERE id = ?", id)
+    return render_template("update.html", birthday=birthday)
